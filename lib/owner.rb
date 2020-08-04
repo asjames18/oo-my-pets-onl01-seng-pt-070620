9fb0d2fd9be943018504d7c885e4e1ff454b87b0
Antonio James <asjames18@gmail.com>
2020-08-04 17:37:25 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,42 @@
+require 'pry'
 class Owner
-  # code goes here
+
+  @@all = []
+
+  attr_accessor :cats, :dogs
+  attr_reader :name
+    def initialize (name)
+      @name = name
+      @species = "human"
+      @@all << self
+      @cats = []
+      @dogs = []
+
+    end
+  # def self
+  #   raise NoMethodError if nothing was raised
+  # end
+    def species
+      @species = "human"
+    end
+
+    def say_species
+      @say_species = "I am a human."
+    end
+   def self.all
+     @@all
+   end
+
+   def self.count
+     self.all.count
+   end
+
+   def self.reset_all
+     self.all.clear
+     end
+
+     def cats
+
+end
+
 end
